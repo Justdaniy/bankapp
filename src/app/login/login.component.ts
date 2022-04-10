e9784cb183a1e3b0f49e72c9fe958dc35e3a50b8
dhanish <dhanish0777@gmail.com>
2022-04-10 23:08:52 +0530

    event binding using

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ acno=""  //its a property of class
 pswd=""
 
   //database
-   database={
+   database:any={
      1000:{acno:1000,uname:"ajith",password:1000,balance:4000},
      1001:{acno:1001,uname:"ismail",password:1001,balance:4005},
      1002:{acno:1002,uname:"sami",password:1002,balance:4002},
@@ -34,8 +34,24 @@ pswdChange(event:any){
   console.log(this.pswd);
   
   }
-
+// login - using event binding
 login(){
-  alert("login clicked")
+  //user entered acno and pswd
+  var acno=this.acno
+  var pswd=this.pswd
+
+let database=this.database
+
+if (acno in database) {
+  if (pswd == database[acno]["password"]) {
+    
+  }
+   else {
+    alert("incorrect password!!!")
+  }
+}
+else{
+  alert("user does not exist!!!")
+}
 }
 }
